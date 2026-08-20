09faed87b9a85aaf54d508fc819d327b884d3e5c
YASH PAL <2004yashpal@gmail.com>
2026-08-20 08:18:43 +0530

    ci: add GitHub Actions workflow and configuration for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://yashpalsince2004.github.io",
+  base: "/Dr.computer",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
